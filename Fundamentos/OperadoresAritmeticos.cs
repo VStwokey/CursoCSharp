7c121896b7ddfe62099f0237fd2edf68ae590b2f
VINICIUS SANTANA DE LIMA PAZ <vinicius.slpaz@senacsp.edu.br>
2023-05-15 16:09:25 -0300

    Curso CSharp exercício sexta

--- Fundamentos/OperadoresAritmeticos.cs
+++ Fundamentos/OperadoresAritmeticos.cs
@@ -11,7 +11,8 @@ namespace CursoCSharp.Fundamentos
     {
         public static void Executar()
         {
-            /*Console.WriteLine("Informe a primeira nota: ");
+            /*
+            Console.WriteLine("Informe a primeira nota: ");
             double n1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Informe a segunda nota: ");
             double n2 = double.Parse(Console.ReadLine());
@@ -23,6 +24,7 @@ namespace CursoCSharp.Fundamentos
             Console.WriteLine($"A sua média final é:  {media}");
             */
 
+            /*
             Console.WriteLine("Informe a primeira nota: ");
             double n1 = double.Parse(Console.ReadLine()) * 3.5;
             Console.WriteLine("Informe a segunda nota: ");
@@ -30,7 +32,21 @@ namespace CursoCSharp.Fundamentos
 
             double media = (n1 + n2) / 11 / 10;
             Console.WriteLine($"A sua média final é:  {media}");
-            
+            */
+
+            Console.WriteLine("Informe seu ID: ");
+            int idfuncionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade de horas trabalhadas: ");
+            int horastrabalhadas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o quanto o SR(A) ganha por hora: ");
+            double salariohora = double.Parse(Console.ReadLine());
+
+            double salario = horastrabalhadas * salariohora;
+
+            Console.WriteLine("O seu número de funcionário é :"+ idfuncionario);
+            Console.WriteLine("O seu salário é: "+salario);
         }
     }
 }
