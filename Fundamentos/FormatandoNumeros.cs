a823c264853667f80ab0e021cc6872227c74dd6a
VINICIUS SANTANA DE LIMA PAZ <vinicius.slpaz@senacsp.edu.br>
2023-05-15 14:53:47 -0300

    Curso CSharp

---
+++ Fundamentos/FormatandoNumeros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class FormatandoNumeros {
+
+        public static void Executar() {
+
+
+            double valor = 15.329;
+            //Arredondar número
+            Console.WriteLine(valor.ToString("F1"));
+        }
+    }
+}
---
+++ Fundamentos/OperadoresAritmeticos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadoresAritmeticos
+    {
+        public static void Executar()
+        {
+            /*Console.WriteLine("Informe a primeira nota: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a segunda nota: ");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a terceira nota: ");
+            double n3 = double.Parse(Console.ReadLine());
+
+            double media = (n1 + n2 + n3) / 3;
+
+            Console.WriteLine($"A sua média final é:  {media}");
+            */
+
+            Console.WriteLine("Informe a primeira nota: ");
+            double n1 = double.Parse(Console.ReadLine()) * 3.5;
+            Console.WriteLine("Informe a segunda nota: ");
+            double n2 = double.Parse(Console.ReadLine()) * 7.5;
+
+            double media = (n1 + n2) / 11 / 10;
+            Console.WriteLine($"A sua média final é:  {media}");
+            
+        }
+    }
+}
